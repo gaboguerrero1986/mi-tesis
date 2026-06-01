73754a1bd9034b0f924008aa03f3a9e0412526b7
gaboguerrero1986 <gguerrero0656@utm.edu.ec>
2026-06-01 17:48:46 -0500

    Update environment.prod.ts

--- proyect_utm/front_utm/src/environments/environment.prod.ts
+++ proyect_utm/front_utm/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend.com', // tu backend real
-  aiServiceUrl: 'https://ai.tu-backend.com',
+  apiUrl: 'https://mi-tesis.onrender.com', // tu backend real
+  aiServiceUrl: 'https://mi-tesis.onrender.com/reports',
   appVersion: '0.0.1',
   enableDebug: false,
   useMocks: false
